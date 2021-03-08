a9ab4a45354a348026a450b4454590ea9a2c415d
Michiyor <joanna.rutkowska96@gmail.com>
2021-03-08 16:24:09 +0100

    hero decorations

--- my-app/src/compoments/Hero/index.js
+++ my-app/src/compoments/Hero/index.js
@@ -1,27 +1,122 @@
-import React from "react";
+import React, {useState} from "react";
 import './style.scss';
 import './Hero.js';
+import icon from "./../../images/settings-icon.png";
+import "../../../node_modules/animate.css/animate.min.css"
 
 function Hero() {
+    let [bounce, setBounce] = useState(false);
     return (
-        <section className='hero container hero__container' id='hero'>
+        <section className='hero  hero__container' id='hero'>
             <h2>Ścieżki rowerowe</h2>
             <nav className="hero__navigation">
                 <ul id="trail__name" className="hero__list">
-                    <li>Stefanka<img src='../../images/settings-icon.png' alt='settings' className='hero__img'/></li>
-                    <li><img src='../../images/settings-icon.png' alt='settings' className='hero__img decoration'/>Twister</li>
-                    <li>Stary zielony<img src='../../images/settings-icon.png' alt='settings' className='hero__img'/>
+                    <li className="hero__list-el">Stefanka
+                        <img
+                            onMouseEnter={() => setBounce(true)}
+                            onMouseLeave={() => setBounce(false)}
+                            src={icon}
+                            alt='settings'
+                            className={`${bounce ? "animate__animated animate__bounceOutRight" : ""} hero__img`}
+                        /></li>
+                    <li className="hero__list-el">
+                        <img
+                            onMouseEnter={() => setBounce(true)}
+                            onMouseLeave={() => setBounce(false)}
+                            src={icon}
+                            alt='settings'
+                            className={`${bounce ? "animate__animated animate__bounceOutLeft" : ""} hero__img`}/>Twister
                     </li>
-                    <li><img src='../../images/settings-icon.png' alt='settings' className='hero__img decoration'/>DH+</li>
-                    <li>Cyganka<img src='../../images/settings-icon.png' alt='settings' className='hero__img'/></li>
-                    <li><img src='../../images/settings-icon.png' alt='settings' className='hero__img decoration'/>Cygan</li>
-                    <li>Daglezjowy<img src='../../images/settings-icon.png' alt='settings' className='hero__img'/></li>
-                    <li><img src='../../images/settings-icon.png' alt='settings' className='hero__img decoration'/>Kamieniołom</li>
-                    <li>Gondola<img src='../../images/settings-icon.png' alt='settings' className='hero__img'/></li>
-                    <li><img src='../../images/settings-icon.png' alt='settings' className='hero__img decoration'/>Dębowiec</li>
-                    <li>Dziabar<img src='../../images/settings-icon.png' alt='settings' className='hero__img'/></li>
-                    <li><img src='../../images/settings-icon.png' alt='settings' className='hero__img decoration'/>Rock'n'Roll</li>
-                    <li>Sahaira<img src='../../images/settings-icon.png' alt='settings' className='hero__img'/></li>
+                    <li className="hero__list-el">Stary zielony
+                        <img
+                            onMouseEnter={() => setBounce(true)}
+                            onMouseLeave={() => setBounce(false)}
+                            src={icon}
+                            alt='settings'
+                            className={`${bounce ? "animate__animated animate__bounceOutRight" : ""} hero__img`}
+                        /></li>
+
+                    <li className="hero__list-el">
+                        <img
+                            onMouseEnter={() => setBounce(true)}
+                            onMouseLeave={() => setBounce(false)}
+                            src={icon}
+                            alt='settings'
+                            className={`${bounce ? "animate__animated animate__bounceOutLeft" : ""} hero__img`}
+                        /> DH+
+                    </li>
+                    <li className="hero__list-el">Cyganka
+                        <img
+                            onMouseEnter={() => setBounce(true)}
+                            onMouseLeave={() => setBounce(false)}
+                            src={icon}
+                            alt='settings'
+                            className={`${bounce ? "animate__animated animate__bounceOutRight" : ""} hero__img`}
+                        /></li>
+                    <li className="hero__list-el">
+                        <img
+                            onMouseEnter={() => setBounce(true)}
+                            onMouseLeave={() => setBounce(false)}
+                            src={icon}
+                            alt='settings'
+                            className={`${bounce ? "animate__animated animate__bounceOutLeft" : ""} hero__img`}/>
+                        Cygan
+                    </li>
+                    <li className="hero__list-el">Daglezjowy
+                        <img
+                            onMouseEnter={() => setBounce(true)}
+                            onMouseLeave={() => setBounce(false)}
+                            src={icon}
+                            alt='settings'
+                            className={`${bounce ? "animate__animated animate__bounceOutRight" : ""} hero__img`}/></li>
+                    <li className="hero__list-el">
+                        <img
+                            onMouseEnter={() => setBounce(true)}
+                            onMouseLeave={() => setBounce(false)}
+                            src={icon}
+                            alt='settings'
+                            className={`${bounce ? "animate__animated animate__bounceOutLeft" : ""} hero__img`}/>
+                        Kamieniołom
+                    </li>
+                    <li className="hero__list-el">Gondola
+                        <img
+                            onMouseEnter={() => setBounce(true)}
+                            onMouseLeave={() => setBounce(false)}
+                            src={icon}
+                            alt='settings'
+                            className={`${bounce ? "animate__animated animate__bounceOutRight" : ""} hero__img`}/></li>
+                    <li className="hero__list-el">
+                        <img
+                            onMouseEnter={() => setBounce(true)}
+                            onMouseLeave={() => setBounce(false)}
+                            src={icon}
+                            alt='settings'
+                            className={`${bounce ? "animate__animated animate__bounceOutLeft" : ""} hero__img`}/>
+                        Dębowiec
+                    </li>
+                    <li className="hero__list-el">Dziabar
+                        <img
+                            onMouseEnter={() => setBounce(true)}
+                            onMouseLeave={() => setBounce(false)}
+                            src={icon}
+                            alt='settings'
+                            className={`${bounce ? "animate__animated animate__bounceOutRight" : ""} hero__img`}/></li>
+                    <li className="hero__list-el">
+                        <img
+                            onMouseEnter={() => setBounce(true)}
+                            onMouseLeave={() => setBounce(false)}
+                            src={icon}
+                            alt='settings'
+                            className={`${bounce ? "animate__animated animate__bounceOutLeft" : ""} hero__img`}                    />
+                    Rock'n'Roll</li>
+                    <li className="hero__list-el">Sahaira
+                        <img
+                        onMouseEnter={() => setBounce(true)}
+                        onMouseLeave={() => setBounce(false)}
+                        src={icon}
+                        alt='settings'
+                        className={`${bounce ? "animate__animated animate__bounceOutRight" : ""} hero__img`} />
+                    </li>
                 </ul>
             </nav>
         </section>
